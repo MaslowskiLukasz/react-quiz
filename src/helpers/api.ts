442cbcf61c5a9e9adcfaeaeb7053190af4fb117e
MaslowskiLukasz <l.maslowski2012@gmail.com>
2025-02-06 17:56:46 +0100

    rename constant

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,15 +1,15 @@
 import { Category, Question } from "../models/models";
 
-const baseUrl = `https://opentdb.com/`;
+const BASE_URL = `https://opentdb.com/`;
 
 export const fetchCategories = async (): Promise<Category[]> => {
-	const response = await fetch(`${baseUrl}/api_category.php`);
+	const response = await fetch(`${BASE_URL}/api_category.php`);
 	const data = await response.json();
 	return data.trivia_categories;
 };
 
 export const fetchQuestions = async (questionsParams: string): Promise<Question[]> => {
-	const response = await fetch(`${baseUrl}/${questionsParams}`);
+	const response = await fetch(`${BASE_URL}/${questionsParams}`);
 	const data = await response.json();
 	return data.results;
 };
