ce5298bb0af34b61b5f73483b389de088553e378
MaslowskiLukasz <l.maslowski2012@gmail.com>
2025-02-06 01:13:56 +0100

    add mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import './App.css'
+import './App.css';
+import '@mantine/core/styles.css';
+import '@mantine/charts/styles.css';
+import { Select } from '@mantine/core';
+
+import { MantineProvider } from '@mantine/core';
 
 function App() {
 
   return (
-    <>
+    <MantineProvider>
       Test
-    </>
+      <Select label="test" placeholder="here we go" data={['test1', 'test2']} />
+    </MantineProvider>
   )
 }
 
