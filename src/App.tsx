f6c13927fef4f75eeacc4eb63219a460e06d3143
MaslowskiLukasz <l.maslowski2012@gmail.com>
2025-02-06 13:16:31 +0100

    delete line

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 import '@mantine/core/styles.css';
 import '@mantine/charts/styles.css';
-
 import { MantineProvider } from '@mantine/core';
 import { MainScreen } from './components/MainScreen';
 
