9d076318186d15911313c479c74729b121728b2a
MaslowskiLukasz <l.maslowski2012@gmail.com>
2025-02-11 01:22:12 +0100

    added displaying number of good answers

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,10 +1,23 @@
 import { PieChart } from "recharts";
 import { AnswerSummary } from "./AnswerSummary";
+import { useContext } from "react";
+import { SelectAnswerContext } from "../App";
 
 export function Results() {
+  const { questions, selectedAnswers } = useContext(SelectAnswerContext);
+
+  const isAnswerCorrect = selectedAnswers.map(
+    (answer: number, index: number) => {
+      const correctAnswer = questions[index].answers.findIndex((item) => item.isCorrect);
+      return answer === correctAnswer;
+    });
+  const numberOfGoodAnswers = isAnswerCorrect.filter((item) => item === true).length;
+  const result = <div>{numberOfGoodAnswers}/{questions.length}</div>
+
   return (
     <>
       <h2>Results</h2>
+      {result}
       <PieChart />
       <AnswerSummary />
     </>
